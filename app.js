d5e2439ba3a201fab1f467aa18490274b52df7ce
Arie Sastra <ariesastra@Aries-MacBook-Pro.local>
2022-02-01 21:39:03 +0700

    deploy to heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// if (process.env.NODE_ENV !== 'production') // deploy purpose
+if (process.env.NODE_ENV !== 'production') // deploy purpose
     require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
